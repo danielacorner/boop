190113474dde5e2e6a5be8fded36c0bdd2ceed1d
daniel.corner <danielcorner7@gmail.com>
2022-07-19 13:52:45 -0400

    checkpoint

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,29 @@ import { LotteryMachine } from "./LotteryMachine";
 
 const { title, href } = shuffleArray([
   {
-    title: "Cygnus - Endless Space 2 Original Soundtrack",
+    title: "Memories (Someone We'll Never Know) - Moon OST",
+    href: "https://www.youtube.com/watch?v=QVuoul47Q_w&list=OLAK5uy_l3rRYn-9yNjarAoHvVh8tNmaOWIeJVEU0&index=5",
+  },
+  {
+    title: "The Nursery - Moon OST",
+    href: "https://www.youtube.com/watch?v=RgVzeqPIt-8&list=OLAK5uy_l3rRYn-9yNjarAoHvVh8tNmaOWIeJVEU0&index=9",
+  },
+  {
+    title: "We're Going Home - Moon OST",
+    href: "https://www.youtube.com/watch?v=O3Kn9mFgr_o&list=OLAK5uy_l3rRYn-9yNjarAoHvVh8tNmaOWIeJVEU0&index=11",
+  },
+  {
+    title: "Cygnus - Endless Space 2 OST",
     href: "https://www.youtube.com/watch?v=PeGv-QtHyaA&list=PLtzah_dj5hUXssVw_j55Fnwjud0fZRle6&index=22",
   },
-  {
-    title: "Worship the Endless - Endless Space 2 Original Soundtrack",
-    href: "https://www.youtube.com/watch?v=uN6JFzeSAz0&list=PLtzah_dj5hUXssVw_j55Fnwjud0fZRle6&index=2",
-  },
-  {
-    title: "Three Quarks in a Row - Endless Space 2 Original Soundtrack",
-    href: "https://www.youtube.com/watch?v=MEvIBLiGDu8&list=PLtzah_dj5hUXssVw_j55Fnwjud0fZRle6&index=17",
-  },
+  // {
+  //   title: "Worship the Endless - Endless Space 2 OST",
+  //   href: "https://www.youtube.com/watch?v=uN6JFzeSAz0&list=PLtzah_dj5hUXssVw_j55Fnwjud0fZRle6&index=2",
+  // },
+  // {
+  //   title: "Three Quarks in a Row - Endless Space 2 OST",
+  //   href: "https://www.youtube.com/watch?v=MEvIBLiGDu8&list=PLtzah_dj5hUXssVw_j55Fnwjud0fZRle6&index=17",
+  // },
 ])[0];
 function App() {
   return (
