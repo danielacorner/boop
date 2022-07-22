da1c4809864f4146ed589257dd8bd2daa6ccd2f2
daniel.corner <danielcorner7@gmail.com>
2022-07-22 16:57:27 -0400

    stars distance

--- src/LotteryMachine.tsx
+++ src/LotteryMachine.tsx
@@ -253,15 +253,13 @@ function Effects(props) {
 function FancyStars() {
   const { viewport } = useThree();
 
+  const maxViewport = Math.max(viewport.width, viewport.height);
   return (
     <Stars
       count={viewport.width > 8 ? 12000 : 10000}
       depth={3}
       factor={0.5}
-      radius={
-        Math.max(viewport.width, viewport.height) *
-        (viewport.width > 8 ? 0.5 : 0.7)
-      }
+      radius={maxViewport * (maxViewport > 16 ? 0.5 : 0.7)}
       fade={true}
     />
   );
